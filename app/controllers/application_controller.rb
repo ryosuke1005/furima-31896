f9d71dc38b4f29696c3afdef18dc13b5f26f62b1
ryosuke1005 <nm1014mn@gmail.com>
2021-02-28 06:38:37 +0900

    ユーザー管理実装、テストコード実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def create
+    @user = user.new(user_params)
+    if @user.save
+      redirect_to controller: :users, action: :index
+    else
+      render "new"
+    end
+  end
+
+
   private
 
   def basic_auth
@@ -7,4 +19,10 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday, :first_name, :last_name, :first_name_kana, :last_name_kana])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password,])
+  end
+  
 end
