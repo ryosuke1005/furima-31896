561b3fa4e957c6847da16499e260f4301b48480c
ryosuke1005 <nm1014mn@gmail.com>
2021-03-08 00:53:18 +0900

    商品出品を保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,25 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase_history
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_date
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explain
+    validates :price
+  end
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+  validates :price, numericality: { only_integer: true, message: "Half-width number." }
+  validates :status_id, numericality: { other_than: 0, message: "Select" }
+  validates :postage_id, numericality: { other_than: 0, message: "Select" }
+  validates :prefecture_id, numericality: { other_than: 0, message: "Select" }
+  validates :shipping_date_id, numericality: { other_than: 0, message: "Select" }
+  validates :category_id, numericality: { other_than: 0, message: "Select" }
 end
