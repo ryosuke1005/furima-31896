b39e966a953d808d7fbb8b997c80331b54c56b27
ryosuke1005 <nm1014mn@gmail.com>
2021-03-21 21:52:10 +0900

    修正2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
-=======
-  devise_for :users
->>>>>>> Stashed changes
   root to: 'items#index'
   resources :items, only: [:index, :new, :create]
 end
