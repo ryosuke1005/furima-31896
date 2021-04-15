cac90c9e78acdd962cf12b65b2cac80436205637
ryosuke1005 <nm1014mn@gmail.com>
2021-04-15 21:00:10 +0900

    修正4　購入機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -57,8 +57,7 @@ class ItemsController < ApplicationController
   end
 
   def set_item_user
-    redirect_to root_path unless current_user.id == @item.user_id
-    redirect_to root_path if current_user.id != @item.user_id
+    redirect_to root_path if current_user.id != @item.user_id || @item.order == nil
   end
 
 end
